d28ffae87ddb282f084cb277122a0ed8c6085ca2
Kyle Tilley <katilley10@gmail.com>
2020-06-08 16:40:44 -0600

    get route controller done

---
+++ server/controllers.js
@@ -0,0 +1,15 @@
+const Score = require('../database/ScoreSchema');
+
+exports.getScores = (req, res) => {
+  Score.find()
+    .then((result) => {
+      res.status(200).send(result);
+    })
+    .catch((err) => {
+      res.status(404).send();
+    });
+};
+
+exports.putScores = (req, res) => {
+  
+}
--- server/index.js
+++ server/index.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 
+const { getScores, putScores } = require('./controllers');
+
 const app = express();
 
 const port = 8153;
 app.listen(port, () => console.log(`listening on ${port}`))
 app.use(cors());
 
-app.get('/start', (req, res) => {
+app.get('/start', getScores);
 
-});
-
-app.put('/result', (req, res) => {
-
-});
+app.put('/result', putScores);
