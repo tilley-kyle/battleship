9a3eb716fb0df1efab6cc8e18a433fa2ddcaf4d4
Kyle Tilley <katilley10@gmail.com>
2020-06-08 16:34:20 -0600

    schema made for keeping track of scores

---
+++ database/ScoreSchema.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/Battleship', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.log('Danger Will Robinson'));
+
+const scoreSchema = new mongoose.Schema({
+  player1: Number,
+  player2: Number,
+});
+
+const Score = mongoose.model('Score', scoreSchema);
+
+module.exports = Score;
