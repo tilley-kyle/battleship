1e59d207d65203fbe6b1bcd89ad2cd2c610a6406
Kyle Tilley <katilley10@gmail.com>
2020-06-11 19:13:32 -0600

    finished setting up the BoardHeader component

--- src/components/BoardHeader.jsx
+++ src/components/BoardHeader.jsx
@@ -4,6 +4,13 @@ const BoardHeader = ({ stage, turn, playerID }) => {
   let filler = '';
   if (stage !== 'battle') {
     filler = `Admiral ${playerID}, set your fleet!`;
+  } else if (stage === 'player ready') {
+    const otherPlayer = playerID === 1 ? 1 : 2;
+    filler = `Waiting on Admiral ${otherPlayer}`;
+  } else if (stage === 'battle' && turn === playerID) {
+    filler = `Admiral ${playerID}, launch your attack!`;
+  } else {
+    filler = `Waiting for Admiral ${turn}'s attack`;
   }
   return (
     <div className="label-title">{filler}</div>
