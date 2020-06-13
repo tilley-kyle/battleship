a8262e7398f7edb1fdd812ffabc056612a04f86d
Kyle Tilley <katilley10@gmail.com>
2020-06-13 11:39:33 -0600

    cleaned up the server

--- server/index.js
+++ server/index.js
@@ -21,33 +21,25 @@ let playerCount = 0;
 io.on('connection', (socket) => {
   playerCount += 1;
   console.log('a user connected, dawg. COUNT: ', playerCount);
-
   socket.emit('join', playerCount)
-
   socket.on('deploy', (state) => {
     socket.broadcast.emit('deploy', state);
   });
-
   socket.on('battle', (toBattle) => {
     socket.broadcast.emit('battle', toBattle);
   });
-
   socket.on('hit', (hitCoord) => {
     socket.broadcast.emit('hit', hitCoord);
   });
-
   socket.on('miss', (missCoord) => {
     socket.broadcast.emit('miss', missCoord)
   });
-
   socket.on('win', (winner) => {
     socket.broadcast.emit('win', winner)
   })
-
   socket.on('restart', () => {
     socket.emit('restart', state);
   });
-
   socket.on('disconnect', () => {
     playerCount -= 1;
     console.log('the user has left, dawg. COUNT: ', playerCount);
