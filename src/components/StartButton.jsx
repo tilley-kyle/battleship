edd10eb00230b30dfe78a6dfa353dfc8c5eb27b3
Kyle Tilley <katilley10@gmail.com>
2020-06-12 19:14:15 -0600

    fixed conditional wording on the deploy button

--- src/components/StartButton.jsx
+++ src/components/StartButton.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const StartButton = ({ stage, handleDeploy }) => {
-  const start = stage === 'setup' ? "start-button-grey" : "start-button-red";
+  let start = stage === 'setup' ? "start-button-grey" : "start-button-red";
   let buttonWords = 'Deploy the Fleet!';
-  if (stage === 'player ready') buttonWords = 'Waitng on Opponent'
+  console.log(stage)
+  if (stage === 'player ready') buttonWords = 'Waitng on Opponent';
+
   return (
     <button className={start} onClick={(e) => handleDeploy(e)}>{buttonWords}</button>
   )
