05f92cd2474e162d26559abcb8abb524a1a3c44a
Kyle Tilley <katilley10@gmail.com>
2020-06-13 09:48:55 -0600

    commenting out database so heroku doesn't have to deal with mongodb

--- database/ScoreSchema.js
+++ database/ScoreSchema.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/Battleship', { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', () => console.log('Danger Will Robinson'));
+// mongoose.connect('mongodb://localhost/Battleship', { useNewUrlParser: true, useUnifiedTopology: true });
+// const db = mongoose.connection;
+// db.on('error', () => console.log('Danger Will Robinson'));
 
-const scoreSchema = new mongoose.Schema({
-  player1: Number,
-  player2: Number,
-});
+// const scoreSchema = new mongoose.Schema({
+//   player1: Number,
+//   player2: Number,
+// });
 
-const Score = mongoose.model('Score', scoreSchema);
+// const Score = mongoose.model('Score', scoreSchema);
 
-module.exports = Score;
+// module.exports = Score;
--- server/controllers.js
+++ server/controllers.js
@@ -1,15 +1,15 @@
-const Score = require('../database/ScoreSchema');
+// const Score = require('../database/ScoreSchema');
 
-exports.getScores = (req, res) => {
-  Score.find()
-    .then((result) => {
-      res.status(200).send(result);
-    })
-    .catch((err) => {
-      res.status(404).send();
-    });
-};
+// exports.getScores = (req, res) => {
+//   Score.find()
+//     .then((result) => {
+//       res.status(200).send(result);
+//     })
+//     .catch((err) => {
+//       res.status(404).send();
+//     });
+// };
 
-exports.putScores = (req, res) => {
-  console.log(req.body);
-}
+// exports.putScores = (req, res) => {
+//   console.log(req.body);
+// }
