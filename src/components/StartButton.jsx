ff3437b21be10b3e843e09d89eeb9c0d9ce619b1
Kyle Tilley <katilley10@gmail.com>
2020-06-12 20:32:56 -0600

    got rid of a console log

--- src/components/StartButton.jsx
+++ src/components/StartButton.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const StartButton = ({ stage, handleDeploy }) => {
   let start = stage === 'setup' ? "start-button-grey" : "start-button-red";
   let buttonWords = 'Deploy the Fleet!';
-  console.log(stage)
   if (stage === 'player ready') buttonWords = 'Waitng on Opponent';
 
   return (
